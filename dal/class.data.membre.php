fc54c9432b0e8e30b91be1dd7df471f5d7277d16
LoganJss <106245977+LoganJss@users.noreply.github.com>
2023-04-12 15:32:39 +0200

    Add files via upload

--- dal/class.data.membre.php
+++ dal/class.data.membre.php
@@ -36,7 +36,8 @@ class Membre extends Data{
     protected $numero_bb;
     protected $date_attestation;
     protected $numera_ppla;
-
+    protected $login;
+    protected $password;
 
     public function selectMembre($num_membre){
         $select = $this->db->prepare("SELECT * FROM membres WHERE num_membre = :num_membre;");
@@ -59,73 +60,6 @@ class Membre extends Data{
         return $select;
     }
     
-    public function insertMembre(){
-        $insert = $this->db->prepare("INSERT INTO membres(num_membre, nom, prenom, adresse, code_postal, ville, num_civil, tel, portable, email, commentaire, date_vm, validite_vm, date_sep, validite_sep, num_badge, num_qualif, profession, date_naissance, lieu_naissance, carte_federale, date_attestion, date_theorique_bb, date_theorique_ppla, date_bb, date_ppla, date_entree, membre_actif, membre_inscrit) VALUES (:num_membre, :nom, :prenom, :adresse, :code_postal, :ville, :num_civil, :tel, :portable, :email, :commentaire, :date_vm, :validite_vm, :date_sep, :validite_sep, :num_badge, :num_qualif, :profession, :date_naissance, :lieu_naissance, :carte_federale, :date_attestion, :date_theorique_bb, :date_theorique_ppla, :date_bb, :date_ppla, :date_entree, :membre_actif, :membre_inscrit);");
-        $insert->execute([
-            'num_membre' => $this->num_membre,
-            'nom' => $this->nom,
-            'prenom' => $this->prenom,
-            'adresse' => $this->adresse,
-            'code_postal' => $this->code_postal,
-            'ville' => $this->ville,
-            'num_civil' => $this->num_civil,
-            'tel' => $this->tel,
-            'portable' => $this->portable,
-            'email' => $this->email,
-            'commentaire' => $this->commentaire,
-            'date_vm' => $this->date_vm,
-            'validite_vm' => $this->validite_vm,
-            'date_sep' => $this->date_sep,
-            'validite_sep' => $this->validite_sep,
-            'num_badge' => $this->num_badge,
-            'num_qualif' => $this->num_qualif,
-            'profession' => $this->profession,
-            'date_naissance' => $this->date_naissance,
-            'lieu_naissance' => $this->lieu_naissance,
-            'carte_federale' => $this->carte_federale,
-            'date_attestion' => $this->date_attestion,
-            'date_theorique_bb' => $this->date_theorique_bb,
-            'date_theorique_ppla' => $this->date_theorique_ppla,
-            'date_bb' => $this->date_bb,
-            'date_ppla' => $this->date_ppla,
-            'date_entree' => $this->date_entree,
-            'membre_actif' => $this->membre_actif,
-            'membre_inscrit' => $this->membre_inscrit
-            
-        ]);
-        $insert = $insert->fetch(PDO::FETCH_ASSOC);
-        if(is_array($insert)) foreach($insert as $key => $value){
-            $this->$key = $value;
-        }
-    }
-    
-    public function updateMembre($num_avion){
-        $update = $this->db->prepare("UPDATE avions SET num_avion = :num_avion, type = :type, taux = :taux, forfait1 = :forfait1, forfait2 = :forfait2, forfait3 = :forfait3, heures_forfait1 = :heures_forfait1, heures_forfait2 = :heures_forfait2, heures_forfait3 = :heures_forfait3, reduction_semaine = :reduction_semaine, immatriculation = :immatriculation WHERE num_avion = :exnum_avion;");
-        $update->execute([
-            'exnum_avion' => $num_avion,
-            'num_avion' => $this->num_avion,
-            'type' => $this->type,
-            'taux' => $this->taux,
-            'forfait1' => $this->forfait1,
-            'forfait2' => $this->forfait2,
-            'forfait3' => $this->forfait3,
-            'heures_forfait1' => $this->heures_forfait1,
-            'heures_forfait2' => $this->heures_forfait2,
-            'heures_forfait3' => $this->heures_forfait3,
-            'reduction_semaine' => $this->reduction_semaine,
-            'immatriculation' => $this->immatriculation,
-        ]);
-        $update = $update->fetch(PDO::FETCH_ASSOC);
-        if(is_array($update)) foreach($update as $key => $value){
-            $this->$key = $value;
-        }
-    }
-    
-    public function deleteMembre(){
-        $delete = $this->db->prepare("DELETE FROM avions WHERE num_avion = :num_avion;");
-        $delete->execute(['num_avion' => $this->num_avion]);
-    }
-    
     public function loginVerify($login){
         $loginVerify = $this->db->prepare("SELECT login FROM membres WHERE login = :login;");
         $loginVerify->execute([
@@ -142,7 +76,7 @@ class Membre extends Data{
         return $signinVerify->fetch()['password'];
     }
 
-    public function agentValide($login){
+    public function membreActif($login){
         $agentValide = $this->db->prepare("SELECT membre_actif FROM membres WHERE login = :login;");
         $agentValide->execute([
             'login' => $login
